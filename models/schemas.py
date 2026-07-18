7dfc1dc2834f7e5fa39ff1edb7101bdee1830faa
meradefit-jpg <meradef.it@gmail.com>
2026-07-19 00:52:08 +0100

    AJOUT interpretation_ia

--- models/schemas.py
+++ models/schemas.py
@@ -32,6 +32,7 @@ class SimulationPoint(BaseModel):
     profit_estime: float
 
 class SimulationResponse(BaseModel):
+    interpretation_ia: str          # <-- AJOUT : pour l'interprétation du modèle IA
     selected_model: str            # <-- AJOUT : pour confirmer quel modèle est affiché
     r_squared: float              # <-- AJOUT : pour la validation du modèle (ex: 0.85)
     diagnostic: str               # ex: "Surexploitation", "Sous-exploité"
